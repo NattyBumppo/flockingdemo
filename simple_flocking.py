2c6c1d5d48de7ee467d4f7e0d2ce47c0af495488
Nat Guy <nattybumppo@gmail.com>
2015-05-23 00:23:42 -0700

    Updated to most recent version

--- simple_flocking.py
+++ simple_flocking.py
@@ -1,3 +1,8 @@
+# Below is found a Python implementation of the Boids flocking
+# algorithm to illustrate concepts discussed in the paper
+# previously presented. Python 2 and the sdl2 module are
+# required for this program to work correctly.
+
 from __future__ import division
 
 import sys
@@ -6,47 +11,49 @@ import sdl2.ext
 import random
 import math
 
+WHITE = sdl2.ext.Color(255, 255, 255)
 GREEN = sdl2.ext.Color(0, 255, 0)
+RED = sdl2.ext.Color(255, 0, 0)
+BLUE = sdl2.ext.Color(0, 0, 255)
 PURPLE = sdl2.ext.Color(255, 0, 255)
+
+numFollowers = 30
 
 maxX = 1024
 maxY = 800
-visionDistance = 150
+visionDistance = 300
 minNeighborDistance = 20
-minFalconDistance = 175
+minAntagonistDistance = 200
 minWallDistance = 40
+leaderWeight = 10
+fov = math.pi / 2
 sepForce = 2.0
 wallSepForce = 4
-alignForce = 0.2
-cohesiveForce = 0.2
-numInteractionPartners = 7 # neighbors to actually consider
-centerAttraction = 0.05
-fov = 2.0 * math.pi
+alignForce = 0.5
+cohesiveForce = 0.5
+numInteractionPartners = 10 # neighbors to actually consider
+centerAttraction = 0.5
 
 sparrowSpeed = 2.5
-falconSpeed = 8
-turnSpeed = math.pi / 16
-
-numSparrows = 60
+falconSpeed = 5
 
 # Draws all of the agents, with their headings, onto the given surface
 def drawAgents(agents, surface):
     pixelview = sdl2.ext.PixelView(surface)
     for agent in agents:
+        # print "Agent at %s, %s" % (agent.posX, agent.posY)
+        # Draw triangle
         normedVelX = agent.velX / agent.speed;
         normedVelY = agent.velY / agent.speed;
-        drawLine(int(agent.posX), int(agent.posY), int(agent.posX + normedVelX * agent.width), int(agent.posY + normedVelY * agent.width), agent.color, pixelview)
+        line(int(agent.posX), int(agent.posY), int(agent.posX + normedVelX * agent.width), int(agent.posY + normedVelY * agent.width), agent.color, pixelview)
     del pixelview
 
 
 # Borrowed from https://gist.github.com/arti95/6264890
-def drawLine(x0, y0, x1, y1, color, pixelview):
-    #print "x0:{} y0:{} x1:{} y1:{}".format(x0, y0, x1, y1)
+def line(x0, y0, x1, y1, color, pixelview):
     """draw a line 
     
     http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm"""
-    #sdl2ext.fill(winsurf, BLACK)
-    #pixelview[event.motion.y][event.motion.x] = WHITE
     
     # dont draw put of screen
     # this check should be in "while true" loop but for some reason it 
@@ -97,13 +104,13 @@ class Agent:
 
     def turnLeft(self, mag):
         # Change heading and velocity direction accordingly
-        self.heading -= mag * turnSpeed
+        self.heading -= mag * math.pi / 16.0
         self.velX = self.speed * math.cos(self.heading)
         self.velY = self.speed * math.sin(self.heading)
 
     def turnRight(self, mag):
         # Change heading and velocity direction accordingly
-        self.heading += mag * turnSpeed
+        self.heading += mag * math.pi / 16.0
         self.velX = self.speed * math.cos(self.heading)
         self.velY = self.speed * math.sin(self.heading)
 
@@ -116,28 +123,23 @@ class Agent:
 
         self.heading = self.heading % (math.pi*2) # Mod 2pi
 
-    def getNeighbors(self, agentList):
-        # Return a list of neighbor agents, where neighbors are within visionDistance
-        # and are within field of view
-        withinFovList = [agent for agent in agentList if self.canSee(agent)]
-        visibleList = []
+    def draw(self):
+        if not self.invisible:
+            self.spriteRenderer.render(self.sprite)
 
-        for agent in withinFovList:
-            if agent.name != 'falcon': # Exclude falcon; only consider fellow sparrows neighbors
-                dist = math.sqrt((self.posX - agent.posX)**2 + (self.posY - agent.posY)**2)
-                if dist < visionDistance:
-                    visibleList.append([agent, dist])
-                visibleList = sorted(visibleList, key=lambda neighbor: neighbor[1]) # Sort by distance
+    def becomeInvisible(self):
+        print "becoming invisible!"
+        self.invisible = True
 
-        # Only return top n closest neighbors, where n is a set number of interaction partners
-        return [neighbor for neighbor, dist in visibleList][:numInteractionPartners]
-
-    def amTooClose(self, agent):
-        minimumDistance = minNeighborDistance
+    def isTouching(self, agent):
         dist = math.sqrt((self.posX - agent.posX)**2 + (self.posY - agent.posY)**2)
-        return (dist < minimumDistance)
+        # print dist
+        if dist < self.width / 2 + agent.width / 2:
+            return True
+        else:
+            return False
 
-    # Check if another agent is in this agent's FOV
+    # Check if an agent is in this agent's FOV
     def canSee(self, agent):
         # First, find the vector to the agent
         vecX = agent.posX - self.posX
@@ -148,20 +150,35 @@ class Agent:
         agentHeading = agentHeading % (2.0 * math.pi)
 
         # Now, find the angle from our heading to that heading
-        angle = (agentHeading - self.heading) % 2 * math.pi
+        angle = (self.heading - agentHeading) % 2 * math.pi
         # If angle is within our FOV, then we can see the agent;
         # otherwise, we cannot
-        if (angle <= fov) or (2.0 * math.pi - angle <= fov):
+        if (angle < fov) or (2.0 * math.pi - angle < fov):
             return True
         else:
             return False
 
-    def avoidFalcon(self, otherAgents):
-        for agent in otherAgents:
-            if agent.name == 'falcon':
-                dist = math.sqrt((self.posX - agent.posX)**2 + (self.posY - agent.posY)**2)          
-                if dist < minFalconDistance:
-                    self.turnInOppositeDirection(sepForce, agent.posX, agent.posY)
+
+    def getNeighbors(self, agentList):
+        # Return a list of neighbor agents, where neighbors are within visionDistance
+        visibleList = []
+        for agent in agentList:
+            dist = math.sqrt((self.posX - agent.posX)**2 + (self.posY - agent.posY)**2)
+            # if dist < neighborDist and self.canSee(agent):
+            if dist < visionDistance:
+                visibleList.append([agent, dist])
+            visibleList = sorted(visibleList, key=lambda neighbor: neighbor[1]) # Sort by distance
+
+        # Only return top n closest neighbors, where n is a set number of interaction partners
+        return [neighbor for neighbor, dist in visibleList][:numInteractionPartners]
+
+    def amTooClose(self, agent):
+        if agent.name == 'antagonist':
+            minimumDistance = minAntagonistDistance
+        else:
+            minimumDistance = minNeighborDistance
+        dist = math.sqrt((self.posX - agent.posX)**2 + (self.posY - agent.posY)**2)
+        return (dist < minimumDistance)
 
     def turnInOppositeDirection(self, mag, agentPosX, agentPosY):
         # Apply steering in opposite direction of agent
@@ -195,7 +212,8 @@ class Agent:
         else: # Otherwise it's straight ahead, so do nothing
             pass
 
-    def avoidWalls(self):
+
+    def flockingLogic(self, agents):
         # Avoid walls
         if self.posY < minWallDistance:
             # Steer away from top wall
@@ -205,21 +223,28 @@ class Agent:
             self.turnInOppositeDirection(wallSepForce, self.posX, maxY) 
 
         if self.posX < minWallDistance:
-            # Steer away from left wall
             self.turnInOppositeDirection(wallSepForce, 0, self.posY)
         elif self.posX > maxX - minWallDistance:
             # Steer away from right wall
             self.turnInOppositeDirection(wallSepForce, maxX, self.posY)
 
-    def maintainSeparation(self, neighbors):
+
+        neighbors = self.getNeighbors(agents)
+        if len(neighbors) == 0:
+            # If there are no neighbors, we're done with flocking logic
+            return
+
+        # Isolate neighbors of same color for special flocking dynamics
+        teamNeighbors = [neighbor for neighbor in neighbors if neighbor.color == self.color]
+
         # Avoid crowding neighbors
         for neighbor in neighbors:
             if self.amTooClose(neighbor):
+                # print '%s is too close to %s!' % (self.name, neighbor.name)
                 self.turnInOppositeDirection(sepForce, neighbor.posX, neighbor.posY)
 
-    def alignToAverageHeading(self, neighbors):
-        # Steer towards average heading of neighbors
-        avgHeading = getAverageHeading(neighbors)
+        # Steer towards average heading of team neighbors
+        avgHeading = getAverageHeading(teamNeighbors)
 
         # Determine which is shorter: the angle it would take to turn left
         # and get to the average heading, or the angle it would take to do
@@ -228,7 +253,6 @@ class Agent:
             angleRight = avgHeading + math.pi * 2 - self.heading
         else:
             angleRight = avgHeading - self.heading
-
         if avgHeading > self.heading:
             angleLeft = self.heading + math.pi * 2 - avgHeading
         else:
@@ -241,52 +265,44 @@ class Agent:
         else:
             pass
 
-    def cohereToAveragePosition(self, neighbors):
-        # Steer towards average position of neighbors
-        avgPosX, avgPosY = getAveragePosition(neighbors)
+        # Steer towards average position of team neighbors
+        avgPosX, avgPosY = getAveragePosition(teamNeighbors)
+
         self.turnInSameDirection(cohesiveForce, avgPosX, avgPosY)
-
-
-    def flockingLogic(self, otherAgents):
-
-        self.avoidWalls()
-
-        self.avoidFalcon(otherAgents)
-
-        neighbors = self.getNeighbors(otherAgents)
-        if len(neighbors) == 0:
-            # If there are no neighbors, we're done with flocking logic
-            return
-
-        self.maintainSeparation(neighbors)
-        self.alignToAverageHeading(neighbors)
-        self.cohereToAveragePosition(neighbors)
 
         # Minor center attraction for effect
         self.turnInSameDirection(centerAttraction, maxX / 2, maxY / 2)
-
 
 def getAveragePosition(agents):
     posXSum = 0
     posYSum = 0
     for agent in agents:
-        posXSum += agent.posX
-        posYSum += agent.posY
+        if agent.name == 'leader':
+            posXSum += agent.posX * leaderWeight
+            posYSum += agent.posY * leaderWeight
+        else:
+            posXSum += agent.posX
+            posYSum += agent.posY
 
-    avgPosX = posXSum / len(agents)
-    avgPosY = posYSum / len(agents)
+    avgPosX = posXSum / (len(agents) + leaderWeight - 1)
+    avgPosY = posYSum / (len(agents) + leaderWeight - 1)
 
     return avgPosX, avgPosY
 
 def getAverageHeading(agents):
+
     xCompSum = 0
     yCompSum = 0
     for agent in agents:
-        xCompSum += math.cos(agent.heading)
-        yCompSum += math.sin(agent.heading)
+        if agent.name == 'leader':
+            xCompSum += math.cos(agent.heading) * leaderWeight
+            yCompSum += math.sin(agent.heading) * leaderWeight
+        else:
+            xCompSum += math.cos(agent.heading)
+            yCompSum += math.sin(agent.heading)
 
-    avgXCompSum = xCompSum / len(agents)
-    avgYCompSum = yCompSum / len(agents)
+    avgXCompSum = xCompSum / (len(agents) + leaderWeight - 1)
+    avgYCompSum = yCompSum / (len(agents) + leaderWeight - 1)
 
     avgHeading = math.atan2(avgYCompSum, avgXCompSum)
 
@@ -302,16 +318,16 @@ def main():
     window = sdl2.ext.Window("Flocking Demo", size=(maxX, maxY))
     window.show()
     winsurf = window.get_surface()
-    
-    falcon = Agent(maxX - 100, maxY - 100, falconSpeed, math.pi / 4.0, GREEN, 30, 30, name='falcon')
 
-    sparrows = []
+    antagonist = Agent(500, 500, falconSpeed, 0, GREEN, 30, 30, name='antagonist')
 
-    for i in range(numSparrows):
-        sparrow = Agent(random.randint(200,maxX - 200), random.randint(200,maxY - 200), sparrowSpeed, random.uniform(0,2*math.pi), PURPLE, 10, 10)
-        sparrows.append(sparrow)
+    followers = []
 
-    allAgents = sparrows + [falcon]
+    for i in range(numFollowers):
+        follower = Agent(random.randint(200,maxX - 200), random.randint(200,maxY - 200), sparrowSpeed, random.uniform(0,2*math.pi), PURPLE, 10, 10)
+        followers.append(follower)
+
+    allAgents = followers + [antagonist]
 
     running = True
     while running:
@@ -322,20 +338,23 @@ def main():
                 break
             if event.type == sdl2.SDL_KEYDOWN:
                 if event.key.keysym.sym == sdl2.SDLK_LEFT:
-                    falcon.turnLeft(2)
+                    antagonist.turnLeft(2)
                 elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
-                    falcon.turnRight(2)
+                    antagonist.turnRight(2)
 
-        for i, sparrow in enumerate(sparrows):
+        sdl2.SDL_Delay(10)
+
+        for i, follower in enumerate(followers):
             otherAgents = allAgents[:i] + allAgents[i+1:]
-            sparrow.flockingLogic(otherAgents)
+            follower.flockingLogic(otherAgents)
 
-        for sparrow in sparrows:
-            sparrow.updatePosition()
-        falcon.updatePosition()
+        for follower in followers:
+            follower.updatePosition()
+        antagonist.updatePosition()
 
         sdl2.ext.fill(winsurf, sdl2.ext.Color(0, 0, 0))
-        drawAgents(allAgents, winsurf)
+        agentsToDraw = followers
+        drawAgents(agentsToDraw + [antagonist], winsurf)
 
         window.refresh()
 
